ee6f844a84bf7fa3731628e79945972a7c538b78
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-26 17:30:24 +0000

    Set version to 6.0.0
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_puzzle/trunk@76762 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,16 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext: "sr_puzzle"
-#
-# Auto generated 21-12-2007 00:03
-#
-# Manual updates:
-# Only the data in the array - anything else is removed by next write.
-# "version" and "dependencies" must not be touched!
-########################################################################
+/***************************************************************
+ * Extension Manager/Repository config file for ext "sr_puzzle".
+ *
+ * Auto generated 26-06-2013 13:28
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Puzzle',
 	'description' => 'Adds a Puzzle content element type.',
 	'category' => 'plugin',
@@ -32,17 +32,21 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'version' => '6.0.0',
 	'_md5_values_when_last_written' => '',
-	'constraints' => array(
-		'depends' => array(
+	'constraints' => 
+	array (
+		'depends' => 
+		array (
 			'php' => '5.3.0-0.0.0',
 			'typo3' => '6.0.0-6.1.99',
 		),
-		'conflicts' => array(
+		'conflicts' => 
+		array (
 			'cc_cbrowse' => '',
 		),
-		'suggests' => array(
-		)
-	)
+		'suggests' => 
+		array (
+		),
+	),
 );
 
 ?>
